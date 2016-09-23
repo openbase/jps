b8d25f012b807f7dd211ee9a1c99daa10d522aa3
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2016-09-23 13:00:07 +0200

    cleanup

--- src/main/java/org/openbase/jps/preset/JPPrefix.java
+++ src/main/java/org/openbase/jps/preset/JPPrefix.java
@@ -26,8 +26,6 @@ package org.openbase.jps.preset;
  * #L%
  */
 import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
 import org.openbase.jps.core.JPService;
 import org.openbase.jps.exception.JPNotAvailableException;
 import org.openbase.jps.tools.FileHandler;
