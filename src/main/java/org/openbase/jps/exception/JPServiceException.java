6aa7d719c1d176be4f83071a5cafcbeedf0cdef0
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-01-10 23:06:19 +0100

    License header 2017 upgrade.

--- src/main/java/org/openbase/jps/exception/JPServiceException.java
+++ src/main/java/org/openbase/jps/exception/JPServiceException.java
@@ -8,7 +8,7 @@ package org.openbase.jps.exception;
  * #%L
  * JPS
  * %%
- * Copyright (C) 2014 - 2016 openbase.org
+ * Copyright (C) 2014 - 2017 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
