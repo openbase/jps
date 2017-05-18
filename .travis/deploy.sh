669f1a2d4d368621ee0afbaf7fe933a24f6ab32d
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-05-19 00:02:02 +0200

    travis java doc deploy tests.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 set -ev
-# start lib deployment  ...
+### start lib deployment  ...
 #mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
 
 
-# start doc deployment ...
-
-# curren folder
-pwd
-#travis home
-echo $HOME
-git status
-
+### start doc deployment ...
 
 # clear existsing doc files 
-#rm -rf docs
-#mkdir docs
+rm -rf docs
+mkdir docs
 
 # build javadoc 
-#mvn javadoc:aggregate  -DreportOutputDirectory=docs
+mvn javadoc:aggregate  -DreportOutputDirectory=docs
 
-# Swich to the Travis build directory, configure git and clone the repo
-#cd $HOME
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "travis-ci"
+# Setup git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
 
 # Commit and Push the Changes
 
-#git add docs
-#git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git status
+git add docs
+git status
+git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git status
 #git push
-#echo ### deployment successfully finished
+echo ### deployment successfully finished
