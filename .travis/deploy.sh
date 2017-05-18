dd8a33c7325faf8f176bbfa2fad1c5be4b5e2543
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-05-18 23:36:53 +0200

    implemented travis auto java doc updater.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 set -ev
-echo ### start deployment...
+echo ### start lib deployment  ...
 mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
+echo ### start doc deployment ...
+
+
+
+rm -rf docs
+mkdir docs
+mvn javadoc:aggregate  -DreportOutputDirectory=docs
+
+# Get to the Travis build directory, configure git and clone the repo
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+# Commit and Push the Changes
+
+git add docs
+git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push
 echo ### deployment successfully finished
