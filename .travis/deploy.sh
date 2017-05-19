ddb476c118099890fed47ebb4047f470d50697ec
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-05-19 02:09:51 +0200

    javadoc deployment disabled

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 set -ev
 ### start lib deployment  ...
-#mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
+mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
 
 
 ### start doc deployment ...
 
 # checkout master branch
-git checkout master
+#git checkout master
 
 # clear existsing doc files 
-rm -rf docs
-mkdir docs
+#rm -rf docs
+#mkdir docs
 
 # build javadoc 
-mvn javadoc:aggregate  -DreportOutputDirectory=docs
+#mvn javadoc:aggregate  -DreportOutputDirectory=docs
 
 # Setup git
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
+#git config --global user.email "travis@travis-ci.org"
+#git config --global user.name "travis-ci"
 
 # Commit and Push the Changes
 
-git status
-git add docs
-git status
-git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git status
+#git status
+#git add docs
+#git status
+#git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+#git status
+
+# setup github access
+#eval "$(ssh-agent -s)"
+#git ssh-add ${env.GITHUB_KEY}
+
 # deploy
-git push
-echo ### deployment successfully finished
+#git push
+### deployment successfully finished
