271c39fda3b70a6b496bf8194e688a4b84240929
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-06-12 12:57:08 +0200

    install script optimized by skipping licensing.

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@ echo "=== clean ${APP_NAME} ===" &&
 mvn clean $@ &&
 clear &&
 echo "=== install and deploy ${APP_NAME} ===" &&
-mvn install -DskipTests $@ &&
+mvn install \
+        -DassembleDirectory=${prefix} \
+        -DskipTests=true \
+        -Dmaven.test.skip=true \
+        -Dlicense.skipAddThirdParty=true \
+        -Dlicense.skipUpdateProjectLicense=true \
+        -Dlicense.skipDownloadLicenses \
+        -Dlicense.skipCheckLicense=true \
+        -Dmaven.license.skip=true $@ &&
 clear &&
-echo "=== ${APP_NAME} is successfully installed==="
+echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
