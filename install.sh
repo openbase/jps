a5f28705a0351dd9222fbfe810220a619621ea1d
Julian Daberkow <jdaberkow@techfak.uni-bielefeld.de>
2017-06-27 15:43:11 +0200

    Modify install and travis script

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ clear &&
 echo "=== clean ${APP_NAME} ===" &&
 mvn clean $@ &&
 clear &&
-echo "=== install and deploy ${APP_NAME} ===" &&
-mvn install -DskipTests $@ &&
+echo "=== install ${APP_NAME} ===" &&
+mvn install \
+        -DskipTests=true \
+        -Dmaven.test.skip=true \
+        -Dlicense.skipAddThirdParty=true \
+        -Dlicense.skipUpdateProjectLicense=true \
+        -Dlicense.skipDownloadLicenses \
+        -Dlicense.skipCheckLicense=true \
+        -Dmaven.license.skip=true $@ &&
 clear &&
-echo "=== ${APP_NAME} is successfully installed==="
+echo "=== ${APP_NAME} is successfully installed ==="
