72b7dc18a9cd328b4b330df9c088f372de2fe738
Divine Threepwood <divine@openbase.org>
2018-04-06 12:31:42 +0200

    Implement JPUsrDirectory predefined property.

---
+++ src/main/java/org/openbase/jps/preset/JPUsrDirectory.java
@@ -0,0 +1,68 @@
+package org.openbase.jps.preset;
+
+/*-
+ * #%L
+ * JPS
+ * %%
+ * Copyright (C) 2014 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.openbase.jps.core.JPService;
+import org.openbase.jps.exception.JPNotAvailableException;
+import org.openbase.jps.exception.JPValidationException;
+import org.openbase.jps.tools.FileHandler;
+
+import java.io.File;
+
+/**
+ *
+ * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
+ */
+public class JPUsrDirectory extends AbstractJPDirectory {
+
+    public final static String[] COMMAND_IDENTIFIERS = {"--usr"};
+
+    public JPUsrDirectory() {
+        super(COMMAND_IDENTIFIERS, FileHandler.ExistenceHandling.Must, FileHandler.AutoMode.Off);
+    }
+
+    @Override
+    public File getParentDirectory() throws JPNotAvailableException {
+        // root folder
+        return new File("/");
+    }
+
+    @Override
+    protected File getPropertyDefaultValue() {
+        return new File("usr");
+    }
+
+    @Override
+    public void validate() throws JPValidationException {
+        if (JPService.testMode()) {
+            setAutoCreateMode(FileHandler.AutoMode.On);
+            setExistenceHandling(FileHandler.ExistenceHandling.Must);
+        }
+        super.validate();
+    }
+
+    @Override
+    public String getDescription() {
+        return "Specifies the global system usr directory which is used for storing variable application data like databases or models.";
+    }
+}
